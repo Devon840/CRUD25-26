a0bb7a8f2c2f5ecd39b9cb9aec03b6cfac6528ad
Devon840 <160247013@student.rnngroup.ac.uk>
2025-09-09 16:54:00 +0100

    delete monkey

--- crudApp/crudApp/settings.py
+++ crudApp/crudApp/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    "crispy_forms",
-    
-    "crispy_bootstrap5",
+    #"crispy_forms",
+    #"crispy_bootstrap5",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
