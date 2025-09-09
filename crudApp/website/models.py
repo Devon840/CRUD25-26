7b709870b5d6ea0bc78e37790a3b81bff9578ec9
Devon840 <160247013@student.rnngroup.ac.uk>
2025-09-09 16:54:15 +0100

    update models

--- crudApp/website/models.py
+++ crudApp/website/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
-
-class Record(models.Model):
-    creation_data = models.DateTimeField(auto_now_add = True)
-
-    first_name = models.CharField(max_length = 100)
-    last_name = models.CharField(max_length = 100)
-    email = models.CharField(max_length = 255)
-    phone = models.CharField(max_length = 20)
-    address = models.CharField(max_length = 255)
-    city = models.CharField(max_length = 255)
-
-def __str__(self):
-    return self.first_name + "" + self.last_name
-=======
 class Record(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
@@ -27,4 +12,3 @@ class Record(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
->>>>>>> e7e30b9bf6e4833e8f25e46e9acad501b9714b39
