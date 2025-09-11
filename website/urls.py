209561a1d05e133b8e380a84fd7b8ab47963cb60
Devon840 <160247013@student.rnngroup.ac.uk>
2025-09-11 09:50:55 +0100

    fixed errors

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+  path('', views.home, name=""),
+  path('login', views.login, name="login"),
+  #path('store', views.store, name="store"),
+  path('register', views.register, name="register"),
+  path('logout', views.logout, name="logout"),
+  path('dashboard', views.dashboard, name="dashboard"),
+  path("create_record",views.create_record, name="create_record"),
+  path('update_record/<int:pk>', views.update_record, name='update_records'),
+  path('record/<int:pk>', views.singular_record, name="record"),
+]
---
+++ website/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm, CreateRecordForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Record
+# Create your views here.
+def home(request):
+    return render(request, 'pages/index.html')
+
+#logout
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+
+
+def login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+            
+    context = {'login_form': form}
+    return render(request, 'pages/login.html', context = context)
+
+# Register a user
+def register(request):
+    # make a form from forms.py
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        # make a form with the users submitted data
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save() # save to database
+            return redirect('login')
+        
+    context = {'form': form}
+    #print(context)
+
+    return render(request, 'pages/register.html', context=context)
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("login")
+
+@login_required(login_url='login')
+def dashboard(request):
+
+    my_records = Record.objects.all()
+    context = {'Records': my_records}
+    return render(request, 'pages/dashboard.html', context=context)
+
+@login_required(login_url='login')
+def create_record(request):
+
+    form = CreateRecordForm()
+    if request.method == 'POST':
+        form = CreateRecordForm(request.POST)
+        if  form.is_valid():
+            form.save()
+            return redirect("dashboard")
+        
+    context = {'create_form': form}
+    return render(request, 'website/create.html', context=context)
+
+
+@login_required(login_url='login')
+def singular_record(request, pk):
+
+    one_record = Record.objects.get(id=pk)
+    context = {"record":one_record}
+
+    return render(request, 'pages/view_record.html', context=context)
+
+def update_record():
+    pass
